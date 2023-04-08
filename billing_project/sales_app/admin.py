fc471a029fcb6ea8e9be538667856c53778c4c58
Shweta <mankarshweta87@gmail.com>
2023-04-08 23:01:26 +0530

    commit

--- billing_project/sales_app/admin.py
+++ billing_project/sales_app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Customer
-# from stocks_app.models import Product
+from stocks_app.models import Product,ProductCategory
+
 
 # Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['customer_id','customer_name','customer_contact','customer_address']
 
 admin.site.register(Customer, CustomerAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_id','product_name','product_cost_per_quantity','product_category','product_quantity','product_total_cost']
+
+admin.site.register(Product, ProductAdmin)
--- billing_project/sales_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('fcus/', views.FakerCustomerView),
-    path('fpro/', views.FakerProductView),
-]
