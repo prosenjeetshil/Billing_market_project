f22cb105c029a467c1e7503b15db2c07144cbf2e
prosenjeetshil <prosenjeetshil@gmail.com>
2023-04-08 18:05:33 +0530

    fix: nested serialization working

--- billing_project/sales_app/serializers.py
+++ billing_project/sales_app/serializers.py
@@ -2,26 +2,23 @@ from rest_framework import serializers
 from .models import Customer, Invoice, InvoiceProduct
 from stocks_app.serializers import ProductSerializer
 
-
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = '__all__'
-
 class InvoiceProductSerializer(serializers.ModelSerializer):
-    # product_invoice = ProductSerializer(many=True)
-
+    
     class Meta:
         model = InvoiceProduct
         fields = '__all__'
 
 class InvoiceSerializer(serializers.ModelSerializer):
-    customer = CustomerSerializer()
-    # product_in_invoice = ProductSerializer(many=True)
-    # products_invoice = InvoiceProductSerializer(many=True)
+    product_in_invoice = InvoiceProductSerializer(many=True, read_only=True)
 
     class Meta:
         model = Invoice
         fields = '__all__'
 
 
+class CustomerSerializer(serializers.ModelSerializer):
+    invoices = InvoiceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Customer
+        fields = '__all__'
