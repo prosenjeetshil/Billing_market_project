4e62e3b9759c536f0a14f11475f4179208403adb
prosenjeetshil <prosenjeetshil@gmail.com>
2023-04-10 12:19:48 +0530

    added corsheader

--- billing_project/billing_project/settings.py
+++ billing_project/billing_project/settings.py
@@ -45,7 +45,12 @@ INSTALLED_APPS = [
     'faker_app',
     'rest_framework',
     'phonenumber_field',
+    'corsheaders',
 ]
+CORS_ORIGIN_WHITELIST = (
+'http://localhost:3000',  
+'http://127.0.0.1:3000',
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'billing_project.urls'
