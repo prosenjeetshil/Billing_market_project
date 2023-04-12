1a6ae7c0175976c0110b68d41e6540effda7383e
Shweta <mankarshweta87@gmail.com>
2023-04-12 15:05:56 +0530

    salescommit

--- billing_market_frontend/src/App.js
+++ billing_market_frontend/src/App.js
@@ -4,44 +4,39 @@ import Daily_sales from './Components/Sales_components/Daily_sales';
 import Monthly_sales from './Components/Sales_components/Monthly_sales';
 import Quaterly_sales from './Components/Sales_components/Quaterly_sales';
 import Weeklysales from './Components/Sales_components/Weeklysales';
-import Sidebar from "./Components/Sidebar";
+
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-<<<<<<< HEAD
+
 import Invoice from './Components/Sales_components/Invoice';
-import Sales_by_category from './Components/Sales_components/Sales_by_categeory';
-import Productlist from './Components/Sales_components/Productlist';
-=======
+
+
 import BillingComponent from './Components/Sales_components/BillingComponent/BillingComponent';
->>>>>>> 3c636532e766dc3e6f9722f563c7ed4c262129f6
+import SalesAsPerProduct from './Components/Sales_components/SalesAsPerProduct';
+import Saleslist from './Components/Sales_components/SalesReport';
+
 
 function App() {
   return (
     
     <>
     <BrowserRouter>
-<<<<<<< HEAD
 
-=======
-    {/* sidebar commented because of some issue with rendering */}
-    {/* <Sidebar> */}
->>>>>>> 3c636532e766dc3e6f9722f563c7ed4c262129f6
+    
     <Routes>
       <Route path='/Daily_Sales' element={<Daily_sales/>}/>
       <Route path='/Monthly_Sales' element={<Monthly_sales/>}/>
       <Route path='/Quaterly_Sales' element={<Quaterly_sales/>}/>
       <Route path='/Weekly_Sales' element={<Weeklysales/>}/>
-<<<<<<< HEAD
+      <Route path='/SalesAsPerProduct/:product_id' element={<SalesAsPerProduct/>}/>
+
       <Route path='/Invoice' element={<Invoice/>}/>
 
-      <Route path='/Product_list' element={<Productlist/>}/>
-    </Routes>
-  
-=======
+      <Route path='/Sales_list' element={<Saleslist/>}/>
+
       <Route path='/billing' element={<BillingComponent/>}/>
     </Routes>
-    {/* </Sidebar> */}
->>>>>>> 3c636532e766dc3e6f9722f563c7ed4c262129f6
-    </BrowserRouter>
+   
+      </BrowserRouter>
   
     </>
   );
--- billing_project/billing_project/settings.py
+++ billing_project/billing_project/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+  
 ]
 
 ROOT_URLCONF = 'billing_project.urls'
